f309a9eb2a1c8d2e6884c3d91c5de8deddbb5514
Mark Taylor <tardinha@gmail.com>
2021-03-10 12:25:05 +1100

    Add embeddable video element

---
+++ src/Models/Elements/ElementVideo.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace NSWDPC\Elemental\Models\FeaturedVideo;
+
+use DNADesign\Elemental\Models\BaseElement;
+use gorriecoe\Embed\Extensions\Embeddable;
+use SilverStripe\Forms\TextField;
+
+class ElementVideo extends BaseElement
+{
+
+    private static $table_name = 'ElementVideo';
+
+    private static $icon = 'font-icon-block-banner';
+
+    private static $singular_name = 'video';
+
+    private static $plural_name = 'videos';
+
+    private static $db = [
+        'AltVideoURL' => 'Varchar(1024)'
+    ];
+
+    private static $embed_folder = 'Uploads/images';
+
+    public function getType()
+    {
+        return _t(__CLASS__ . '.BlockType', 'Video');
+    }
+
+    private static $extensions = [
+        Embeddable::class,
+    ];
+
+    /**
+     * List the allowed included embed types.  If null all are allowed.
+     * @var array
+     */
+    private static $allowed_embed_types = [
+        'video'
+    ];
+
+    /**
+     * Defines tab to insert the embed fields into.
+     * @var string
+     */
+    private static $embed_tab = 'Main';
+
+
+    public function getCMSFields()
+    {
+        $fields = parent::getCMSFields();
+        $fields->removeByName(['EmbedImage', 'EmbedDescription']);
+        $fields->addFieldToTab('Root.Main', TextField::create('AltVideoURL', 'Alternate video with audio captions enabled'));
+        return $fields;
+    }
+
+}
